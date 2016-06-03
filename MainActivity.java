0d76bc6ec82c7b84f684c355b39071d190d97eef
ChoiJiYoung <bysos7@naver.com>
2016-06-03 23:21:52 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package org.androidtown.bbokbbok_bubble;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
